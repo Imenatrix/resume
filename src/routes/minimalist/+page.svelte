8a1adc2e3f6f9cb3f37420a84b17769ca4ed1855
Ruan Gomes <ruangomesdorosario@gmail.com>
2024-05-22 09:58:30 -0300

    Implement resume index

--- src/routes/minimalist/+page.svelte
+++ src/routes/minimalist/+page.svelte
@@ -1,116 +1,38 @@
-<script lang='ts'>
-	import resume from '$lib/resume/portuguese.json'
+<script lang="ts">
+    import { loadResumes } from '$lib/utils/resume'
+    import { snakeToTitle } from '$lib/utils/strings'
+
+    const PT_RESUMES_FOLDER = 'src/lib/resume/portuguese'
+    const EN_RESUMES_FOLDER = 'src/lib/resume/english'
+
+    const portugueseResumes = loadResumes(PT_RESUMES_FOLDER)
+    const englishResumes = loadResumes(EN_RESUMES_FOLDER)
 </script>
 
-<div class="title">
-	<h1>{resume.info.name}</h1>
-	<hr>
-</div>
-<div class="resume">
-	<aside>
-		<section>
-			<h2>Contato</h2>
-			<p>
-				<b>Endereço:</b>
-				{`${resume.contact.adress.street}, ${resume.contact.adress.number}, ${resume.contact.adress.district}, ${resume.contact.adress.city}, ${resume.contact.adress.state}`}
-			</p>
-			<p>
-				<b>Telefone:</b>
-				{`(${resume.contact.phone.ddd}) ${resume.contact.phone.number.slice(0, 5)}-${resume.contact.phone.number.slice(5)}`}
-			</p>
-			<p>
-				<b>E-mail:</b>
-				{resume.contact.email}
-			</p>
-			<p>
-				<b>LinkedIn:</b>
-				{resume.contact.linkedin}
-			</p>
-			<p>
-				<b>GitHub:</b>
-				{resume.contact.github}
-			</p>
-		</section>
-		<section>
-			<h2>Principais Skills</h2>
-			<ul>
-				{#each resume.skills as skill}
-					<li>{skill}</li>
-				{/each}
-			</ul>
-		</section>
-		<section>
-			<h2>Interesses</h2>
-			<ul>
-				{#each resume.interests as interest}
-					<li>{interest}</li>
-				{/each}
-			</ul>
-		</section>
-	</aside>
-	<hr>
-	<main>
-		
-		<section>
-			<h2>Experiências</h2>
-			{#each resume.experience as experience}
-				<div>
-					<b>{experience.position}</b> <br>
-					{#if experience.start.month && experience.end.month}
-						<i>{`${experience.company} | ${experience.start.month.toString().padStart(2, '0')}/${experience.start.year} - ${experience.end.month.toString().padStart(2, '0')}/${experience.end.year}`}</i>	
-					{:else}
-						<i>{`${experience.company} | ${experience.start.year} - ${experience.end.year}`}</i>
-					{/if}
-					<p>{experience.description}</p>
-				</div>
-			{/each}
-		</section>
-		<section>
-			<h2>Escolaridade</h2>
-			{#each resume.education as education}
-				<div>
-					<b>{education.course}</b> <br>
-					<i>{`${education.institution} | ${education.start?.year ? education.start.year + ' - ' : ''}${education.end.year}`}</i>
-					<p></p>
-				</div>
-			{/each}
-		</section>
-		<section>
-			<h2>Cursos</h2>
-			{#each resume.courses as education}
-				<div>
-					<b>{education.course}</b> - <i>{`${education.end.year}`}</i>
-					<p></p>
-				</div>
-			{/each}
-		</section>
-		<section>
-			<h2>Premiações</h2>
-			{#each resume.prizes as prize}
-				<div>
-					<b>{prize.prize}</b> - <i>{prize.event}</i>
-					<p></p>
-				</div>
-			{/each}
-		</section>
-	</main>
-</div>
+<head>
+    <title>Resumes</title>
+</head>
 
-<style>
-	.title {
-		margin-left: 1em;
-		margin-right: 1em;
-	}
-	.resume {
-		display : flex;
-	}
-	.resume > * {
-		margin : 1em;
-	}
-	section {
-		margin-bottom : 3em;
-	}
-	ul {
-		list-style-type: circle;
-	}
-</style>
+<h1>Resumes</h1>
+
+<h2>Portuguese</h2>
+<ul>
+    {#each Object.keys(portugueseResumes) as resume_name}
+        <li>
+            <a href="/minimalist/{resume_name}/portuguese">
+                {snakeToTitle(resume_name)}
+            </a>
+        </li>
+    {/each}
+</ul>
+
+<h2>English</h2>
+<ul>
+    {#each Object.keys(englishResumes) as resume_name}
+        <li>
+            <a href="/minimalist/{resume_name}/english">
+                {snakeToTitle(resume_name)}
+            </a>
+        </li>
+    {/each}
+</ul>
